92cedce1b3f77d3f2bdbceb572b56e3f0942fe01
KyeongmoonKim <kkm8031@snu.ac.kr>
2019-06-05 06:03:04 -0700

    too slow

--- pj2.py
+++ pj2.py
@@ -1,7 +1,7 @@
 import numpy as np
 from hmmlearn import hmm
 import csv
-import datetime
+import random
 np.random.seed(42)
 
 x_train = open('x_train.csv', 'r', encoding='utf-8')
@@ -13,6 +13,26 @@ train_size = 1000
 test_size = 1000
 model_list = []
 components_size = 20
+my_filter1 = [0, 0, 1.0, 1.0] #horizon
+my_filter2 = [0, 1.0, 0, 1.0] #vertical
+my_filter_size = 2;
+my_target_size = 28;
+
+def conv_one_step(target, filter, row, col, target_size, filter_size): #(row, col) of target, square t_s*t_s, f_s*f_s
+	ret = 0
+	for i in range(0, filter_size):
+		for j in range(0, filter_size):
+			ret = ret + target[(row + i) * target_size + col+j] * filter[i * filter_size+j]
+	return ret
+			
+def conv(target, filter, target_size, filter_size):
+	ret = []
+	n = target_size - filter_size + 1
+	for i in range(0, n):
+		for j in range(0, n):
+			value = conv_one_step(target, filter, i, j, target_size, filter_size)
+			ret.append([value])
+	return ret
 
 for i in range(0, 10):
 	temp = hmm.GaussianHMM(n_components = components_size, covariance_type="diag")
@@ -21,7 +41,20 @@ for i in range(0, 10):
 	model_list.append(temp)
 
 num = 0
-#lengths = [2]*143
+
+def pair_list(list1, list2): #list1, list2 same len
+	if(len(list1)!=len(list2)):
+		return []
+	ret = []
+	for i in range(0, len(list1)):
+		ret.append(list1[i])
+		ret.append(list2[i])
+	return ret
+
+lengths = [2]*676
+
+
+print("train start")
 
 for line in train_reader:
 	if(num==0):
@@ -33,9 +66,10 @@ for line in train_reader:
 		print(num)
 	sub_list = line[1:len(line)]
 	sub_list = list(map(lambda i : int(i), sub_list))
-	X = np.array(sub_list).reshape(-1, 1)
-	#print(X)
-	model_list[int(line[0])].fit(X)
+	X1 = conv(sub_list, my_filter1, my_target_size, my_filter_size)
+	X2 = conv(sub_list, my_filter2, my_target_size, my_filter_size)
+	X = np.array(pair_list(X1,X2))
+	model_list[int(line[0])].fit(X, lengths)
 	num = num+1
 answer = 0
 n = 0
@@ -50,10 +84,12 @@ for line in test_reader:
 		print(n)
 	sub_list = line[1:len(line)]
 	sub_list = list(map(lambda i : int(i), sub_list))
-	X = np.array(sub_list).reshape(-1, 1)
+	X1 = conv(sub_list, my_filter1, my_target_size, my_filter_size)
+	X2 = conv(sub_list, my_filter2, my_target_size, my_filter_size)
+	X = np.array(pair_list(X1,X2))
 	Y = []
 	for i in range(0, 10):
-		Y.append(model_list[i].score(X))
+		Y.append(model_list[i].score(X, lengths))
 	inference = Y.index(max(Y))
 	if(inference == int(line[0])):
 		answer = answer + 1
